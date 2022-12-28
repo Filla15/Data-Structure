dadcc88db4b97e3bb3d9efa29fda502a8e8987b3
Filla Firontina <120008072+Filla15@users.noreply.github.com>
2022-12-28 19:59:57 +0700

    Set(kata acak).py

---
+++ set(kata acak).py
@@ -0,0 +1,33 @@
+Days=set(["a","b","c","d","e","f","g"])
+Months={"ab","bc","cd"}
+Dates={1,34,45,2,100}
+print(Days)
+print(Months)
+print(Dates)
+
+#menggunakan kurung kurawal
+himpunan_mhsuniss = {'faqih','ageng','viki','filla'}
+print(himpunan_mhsuniss)
+
+#mengkonversi list ke dalam set
+himpunan_buah =set(['mangga','apel','anggur','semangka'])
+print(himpunan_buah)
+
+#set dengan tipe data yang berbeda-beda
+set_campuran = {'manusia','hewan',5,True, ('A','B')}
+print(set_campuran)
+
+print("Menambah set baru")
+himpunan_abjad ={'a','b','c'}
+print(himpunan_abjad)
+
+#menambah satu-satu
+himpunan_abjad.add('d')
+himpunan_abjad.add('e')
+print(himpunan_abjad)
+
+#menambah lebih dari satu anggota sekaligus
+himpunan_abjad.update({'f','g'})
+print(himpunan_abjad)
+
+
