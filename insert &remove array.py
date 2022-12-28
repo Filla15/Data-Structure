586968db50afe600cc5e06391ba65f13d37b1c16
Filla Firontina <120008072+Filla15@users.noreply.github.com>
2022-12-28 19:58:23 +0700

    Insert & remove.py

---
+++ insert &remove array.py
@@ -0,0 +1,13 @@
+from array import *
+array1 = array('i',[10,20,30,40,50])
+array1.insert(1,60)
+for x in array1:
+ print(x)
+ 
+array2=array('i',[10,20,30,40,50])
+array2.remove(40)
+for x in array2:
+ print(x)
+
+
+
